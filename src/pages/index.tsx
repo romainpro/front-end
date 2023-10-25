cf166d8e8028ffdf22d819eb77d71d7dda744b32
romain <romain.casagrande.pro@gmail.com>
2023-10-25 13:59:17 +0200

    tailwind_config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,23 @@ import { Typography } from "@/ui/design-system/typography/typography";
 
 export default function Home() {
   return (
-  <>
+  <div className="space-y-5">
   
-  <Typography variant="display">
+  <Typography variant="display" component="div">
     coders Romain
   </Typography>
-  </>
+  <Typography  theme="primary" variant="h1" component="div">
+    coders Romain
+  </Typography>
+  <Typography theme="secondary" variant="lead" component="div">
+    coders Romain
+  </Typography>
+  <Typography variant="body-sm" component="div">
+    coders Romain
+  </Typography>
+  <Typography variant="caption4" weight="medium" component="div">
+    coders Romain
+  </Typography>
+  </div>
   )
 }
--- src/ui/design-system/typography/typography.tsx
+++ src/ui/design-system/typography/typography.tsx
@@ -4,6 +4,7 @@ interface Props {
   variant?:
     | "display"
     | "h1"
+    | "h2"
     | "h3"
     | "h4"
     | "h5"
@@ -16,7 +17,7 @@ interface Props {
     | "caption3"
     | "caption4";
   component?: "h1" | "h2" | "h3" | "h4" | "h5" | "div" | "p" | "span";
-  theme?: "black" | "grey" | "white" | "primairy" | "secondary";
+  theme?: "black" | "gray" | "white" | "primary" | "secondary";
   weight?: "regular" | "medium";
   className?: string;
   children: React.ReactNode;
@@ -30,16 +31,77 @@ export const Typography = ({
   className,
   children,
 }: Props) => {
-  let variantStyles: string = "div";
+  let variantStyles: string = "",colorStyles:string ="";
 
-  switch (variant) {
-    case "display":
-      variantStyles = "text-red-500";
+  switch (theme) {
+    case "black"://default
+      colorStyles="text-gray"
       break;
-    case "h1":
-      variantStyles = "text-green-500";
+    case "gray":
+      colorStyles="text-gray-700"
+      break;
+    case "white":
+      colorStyles="text-white"
+      break;
+    case "primary":
+      colorStyles="text-primary"
+      break;
+    case "secondary":
+      colorStyles="text-secondary"
       break;
   }
 
-  return <Component className={clsx(variantStyles, weight==="medium" && "font-medium")}>{children}</Component>;
+  switch (variant) {
+    case "display":
+      variantStyles = "text-8xl";
+      break;
+    case "h1":
+      variantStyles = "text-7xl";
+      break;
+    case "h2":
+      variantStyles = "text-6xl";
+      break;
+    case "h3"://DEFAULT
+      variantStyles = "text-5xl";
+      break;
+    case "h4":
+      variantStyles = "text-4xl";
+      break;
+    case "h5":
+      variantStyles = "text-3xl";
+      break;
+    case "lead":
+      variantStyles = "text-2xl";
+      break;
+    case "body-lg":
+      variantStyles = "text-lg";
+      break;
+    case "body-base":
+      variantStyles = "text-base";
+      break;
+    case "body-sm":
+      variantStyles = "text-sm";
+      break;
+    case "caption1":
+      variantStyles = "text-caption1";
+      break;
+    case "caption2":
+      variantStyles = "text-caption2";
+      break;
+    case "caption3":
+      variantStyles = "text-caption3";
+      break;
+    case "caption4":
+      variantStyles = "text-caption4";
+      break;
+  }
+
+  return (
+  <Component 
+    className={clsx(
+      variantStyles,
+      colorStyles,
+      weight==="medium" && "font-medium",
+      className,
+      )}>{children}</Component>);
 };
