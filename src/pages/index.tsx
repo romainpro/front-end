a7475409f68cb3d0609bd0f1b9e9e2e28d399f71
romain <romain.casagrande.pro@gmail.com>
2023-10-25 10:47:16 +0200

    config_firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { Typography } from "@/ui/design-system/typography/typography";
+
+
 export default function Home() {
   return (
   <>
+  
+  <Typography variant="display">
+    coders Romain
+  </Typography>
   </>
   )
 }
