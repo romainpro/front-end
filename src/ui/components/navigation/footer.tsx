a9e2238f83efdfb1332ea665ba0d041abaecfd08
romain <romain.casagrande.pro@gmail.com>
2023-10-27 18:26:37 +0200

    footer_logo

--- src/ui/components/navigation/footer.tsx
+++ src/ui/components/navigation/footer.tsx
@@ -3,6 +3,19 @@ import { Container } from "../container/container"
 import Image from "next/image"
 
 export const Footer=()=>{
+
+    const currentYear=new Date().getFullYear();
+
+    const footerNavigationList = (
+    <div>
+        <ul>
+            <li>t</li>
+            <li>t</li>
+            <li>t</li>
+        </ul>
+    </div>
+    );
+
     return(
         <div className="bg-gray">
             <Container className="flex justify-between pt-16">
@@ -20,7 +33,7 @@ export const Footer=()=>{
                     >
                         Abonne-toi a la chaine !
                     </Typography>
-                    <a href="https://youtube.com" target="_blank">
+                    <a href="https://youtube.com" target="_blank" rel="noopener" aria-label="Youtube">
                         <Image
                             src="/assets/svg/YTB.svg"
                             width={229}
@@ -29,7 +42,22 @@ export const Footer=()=>{
                         />
                     </a>
                 </div>
-                <div className=""></div>
+                <div className="">
+                    {footerNavigationList}
+                </div>
+            </Container>
+            <Container className="pt-9 pb-11 space-y-11">
+                <hr className="text-gray-800"/>
+                <div className="flex items-center justify-between">
+                    <Typography variant="caption4" theme="gray">
+                        {`Copyright © ${currentYear} | Propulsed by`} {""}
+                        <a href="https://www.linkedin.com/in/romain-casagrande-9ba451271/" target="_blank" rel="noopener" aria-label="Linkedin" className="underline">
+                            Romain casgrande
+                        </a>
+                        {`  - Remote Romain` }
+                    </Typography>
+                    <div className=""></div>
+                </div>
             </Container>
         </div>
     )
