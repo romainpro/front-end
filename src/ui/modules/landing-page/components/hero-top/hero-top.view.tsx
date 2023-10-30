35e1fea54bd169c74369d82be118620f05a377e8
romain <romain.casagrande.pro@gmail.com>
2023-10-30 13:14:23 +0100

    add_Layout landing-h1,p-and-btn

--- src/ui/modules/landing-page/components/hero-top/hero-top.view.tsx
+++ src/ui/modules/landing-page/components/hero-top/hero-top.view.tsx
@@ -1,12 +1,24 @@
 import { Container } from "@/ui/components/container/container"
+import { Button } from "@/ui/design-system/button/button"
+import { Typography } from "@/ui/design-system/typography/typography"
 import Image from "next/image"
 
 
 export const HeroTopView=()=>{
     return(
         <Container className="relative pt-40  pb-52 overflow-hidden " >
-            <div>
-
+            <div className="w-full max-w-2xl space-y-5">
+                    <Typography variant="h1" component="h1" className="mx-w-lg">
+                    Rejoins Romain codeurs !
+                    </Typography>
+                    <Typography variant="body-lg" theme="gray" component="p" className="mx-w-xl">
+                    Ici, on se prend pas la tête, mais on code comme des bêtes !           
+                    Rejoins notre tribu de singes codeurs, partage tes projets les plus fous et fais-toi de nouveaux amis développeurs.
+                    </Typography>
+                    <div className="space-x-5 pt-2.5">
+                        <Button baseUrl="">Commencer</Button>
+                        <Button baseUrl="" variant="secondary">En savoir plus</Button>
+                    </div>
             </div>
             <Image
             src="/assets/svg/rocket.svg"
