5827b38e48b6cd5e113d68f6872fe0a79a788b59
romain <romain.casagrande.pro@gmail.com>
2023-10-31 12:53:25 +0100

    container_slack_img_typo

---
+++ src/ui/modules/landing-page/components/coders-monkeys-slack/coders-monkeys-slack.view.tsx
@@ -0,0 +1,33 @@
+import { LinkTypes } from "@/lib/link-types";
+import { Container } from "@/ui/components/container/container";
+import { Button } from "@/ui/design-system/button/button";
+import { Logo } from "@/ui/design-system/logo/logo";
+import { Typography } from "@/ui/design-system/typography/typography";
+import Image from "next/image";
+
+export const CodersMonkeusSlackView = () => {
+  return (
+    <Container className="flex justify-between gap-5">
+      <div className=" flex flex-col justify-center max-w-2xl space-y-5">
+        <div className=" flex items-center gap-2">
+          <Logo size="very-small" />
+          <Typography variant="caption2" component="span" weight="medium">
+            coders Romain
+          </Typography>
+        </div>
+        <Typography variant="h2" component="h2">
+          Rejoins-nous sur le Slack des Singes codeurs
+        </Typography>
+        <Typography variant="body-lg" component="p" theme="gray" className="max-w-lg">
+        Rejoins-nous et obtiens de l'aide, des conseils et pourquoi pas des nouveaux potes !
+        </Typography>
+        <Button baseUrl="/#" linktype={LinkTypes.EXTERNAL}>
+          Rejoindre le groupe d'aide
+        </Button>
+      </div>
+      <div className="relative w-[600px] h-[600px]">
+        <Image fill src={"assets/svg/ico_slack.svg"} alt="Group slack" />
+      </div>
+    </Container>
+  );
+};
--- src/ui/modules/landing-page/landing-page.view.tsx
+++ src/ui/modules/landing-page/landing-page.view.tsx
@@ -1,3 +1,4 @@
+import { CodersMonkeusSlackView } from "./components/coders-monkeys-slack/coders-monkeys-slack.view"
 import { Featured } from "./components/featured/featured.view"
 import { HeroTopView } from "./components/hero-top/hero-top.view"
 
@@ -5,5 +6,6 @@ export const LandingPageView=()=>{
     return <>
     <HeroTopView/>
     <Featured/>
+    <CodersMonkeusSlackView/>
     </>
 }
