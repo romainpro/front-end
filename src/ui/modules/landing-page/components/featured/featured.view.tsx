ba03c08b6e7723144c4aaf8d520479eaf5416f2d
romain <romain.casagrande.pro@gmail.com>
2023-10-31 12:24:15 +0100

    add_feature_img_tiltle_descrip

--- src/ui/modules/landing-page/components/featured/featured.view.tsx
+++ src/ui/modules/landing-page/components/featured/featured.view.tsx
@@ -1,6 +1,9 @@
 import { Container } from "@/ui/components/container/container";
+import { SocialNetworksButton } from "@/ui/components/navigation/social-networks-button";
+import { Button } from "@/ui/design-system/button/button";
 import { Typography } from "@/ui/design-system/typography/typography";
 import Image from "next/image";
+import { RiArrowRightLine } from "react-icons/ri";
 import {v4 as uuidv4} from "uuid"
 
 interface FeacturesListeInterface{
@@ -12,7 +15,7 @@ interface FeacturesListeInterface{
 
 const featuresData=[
     {
-        imagePath:"/assets/svg/Group 18.svg",
+        imagePath:"/assets/svg/floppy.svg",
         imageAlt:"disquette",
         tiltle:"Ressources",
         description:"Consulte et partage des ressources pour les devs",
@@ -44,7 +47,11 @@ export const Featured=()=>{
 
     const featuredList=featuresData.map((feature)=>(
         <div key={uuidv4()} className="flex flex-col items-center justify-center bg-white rounded p-7">
-            <div className= " relative w-[130px] h-[130px] rounded-full mb-6 p-10 bg-gray-400 ">
+            <div className= " relative w-[130px] h-[130px] rounded-full mb-6 p-10 overflow-hidden ">
+                <Image fill
+                src={feature.imagePath}
+                alt={feature.imageAlt}
+                className="object-scale-down blur-2xl"/>
                 <Image fill
                 src={feature.imagePath}
                 alt={feature.imageAlt}
@@ -65,7 +72,25 @@ export const Featured=()=>{
         <div className="bg-gray-300">
             <Container className="grid grid-cols-12 gap-24 py-24">
                 <div className=" grid  grid-cols-2 col-span-7 gap-7 ">{featuredList}</div>
-                <div className=" col-span-5 "></div>
+                <div className=" flex flex-col justify-between gap-10 col-span-5 ">
+                    <div>
+                        <Typography variant="h2" component="h2" className="mb-5">
+                        L'endroit le plus cool pour devenir développeur
+                        </Typography>
+                        <Typography variant="body-lg" component="p" theme="gray" className="mb-8">
+                        Du partage, des connexions et des formations notre app gère tout ça pour toi. 
+                        Rejoins la communauté et grimpe en grade. 
+                        Let's go !
+                        </Typography>
+                        <Button variant="secondary" baseUrl="/#" icon={{icon:RiArrowRightLine}} iconPosition="right" >Commencer</Button>
+                    </div>
+                    <div>
+                    <Typography variant="caption3" theme="gray" component="div" className="mb-4">
+                    Nos réseaux sociaux
+                        </Typography>
+                        <SocialNetworksButton/>
+                    </div>
+                </div>
             </Container>
         </div>
     );
