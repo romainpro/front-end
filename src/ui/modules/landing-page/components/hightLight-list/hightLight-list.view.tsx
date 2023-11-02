894fe618a2de092994630afa140bafa57e359b9b
romain <romain.casagrande.pro@gmail.com>
2023-11-02 10:55:11 +0100

    add_hightlight-list

--- src/ui/modules/landing-page/components/hightLight-list/hightLight-list.view.tsx
+++ src/ui/modules/landing-page/components/hightLight-list/hightLight-list.view.tsx
@@ -1,30 +1,50 @@
-import { Container } from "@/ui/components/container/container"
-import { Typography } from "@/ui/design-system/typography/typography"
-import Image from "next/image"
-import { RiCheckboxCircleFill, RiCheckboxCircleLine } from "react-icons/ri"
+import { Container } from "@/ui/components/container/container";
+import { Button } from "@/ui/design-system/button/button";
+import { Typography } from "@/ui/design-system/typography/typography";
+import Image from "next/image";
+import { RiArrowRightCircleLine, RiCheckboxCircleLine } from "react-icons/ri";
 
-export const HightlightListView = () =>{
+export const HightlightListView = () => {
+  return (
+    <Container className="py-24 sapce-y-10">
+      <div className="flex justify-center gap-24">
+        <div className="w-[520px] h-[350px] relative mt-10">
+          <Image fill src="/assets/svg/cake.svg" alt="cake" />
+        </div>
+        <div className="max-w-md space-y-7">
+          <Typography variant="h3" component="h2">
+            De novice à développeur en un clin d'œil !
+          </Typography>
+         <ListPoint>Progresse rapidement.</ListPoint>
+         <ListPoint>Inspire-toi.</ListPoint>
+         <ListPoint>Gagne de l'assurance.</ListPoint>
+        <div className="relative">
+          <Button baseUrl="/#" icon={{icon: RiArrowRightCircleLine }} 
+          iconPosition="right">
+            Let's go
+          </Button>
+        </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-    return(
-        <Container className="py-24 sapce-y-10">
-            <div className="flex justify-center gap-24">
-                <div className="w-[520px] h-[350px] relative mt-10">
-                    <Image fill src="/assets/svg/cake.svg" alt="cake"/>
-                </div>
-                <div className="max-w-md sapce-y-7">
-                    <Typography variant="h3" component="h2">
-                    De novice à développeur en un clin d'œil !
-                    </Typography>
-                    <div className="space-y-3">
-                        <div className="flex items-center gap-2">
-                            <RiCheckboxCircleLine size={24} className="mt-1 text-secondary"/>
-                            <Typography variant="body-lg" component="span">
-                            Progresse rapidement.
-                                </Typography>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Container>
-    )
+interface Props{
+  children:React.ReactNode
+}
+
+
+const ListPoint =({children}:Props)=>{
+
+  return(
+    <div className="space-y-3">
+    <div className="flex items-center gap-2">
+      <RiCheckboxCircleLine size={24} className="mt-1 text-secondary" />
+      <Typography variant="body-lg" component="span">
+       {children}
+      </Typography>
+    </div>
+  </div>
+  )
 }
