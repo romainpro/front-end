6db20a0a4c7db60363c920927b9e7f09e12437d2
romain <romain.casagrande.pro@gmail.com>
2023-11-03 12:35:30 +0100

    call-to-action-view_refact-btn-success

--- src/ui/design-system/call-to-action.view/call-to-action.view.tsx
+++ src/ui/design-system/call-to-action.view/call-to-action.view.tsx
@@ -1,22 +1,31 @@
-import { Container } from "@/ui/components/container/container"
-import { Typography } from "../typography/typography"
-import { Button } from "../button/button"
-import { LinkTypes } from "@/lib/link-types"
+import { Container } from "@/ui/components/container/container";
+import { Typography } from "../typography/typography";
+import { Button } from "../button/button";
+import { LinkTypes } from "@/lib/link-types";
+import Image from "next/image";
 
-export const CallToActionView =()=>{
-    return(
-        <div className="relative overflow-hidden bg-primary">
-            <Container className="py-20">
-                <div>
-                    <Typography variant="h2" theme="white" component="h2">
-                    N'attend pas pour développer tes compétences...
-                    </Typography>
-                    <Button variant="success" baseUrl="/#" linktype={LinkTypes.EXTERNAL}>
-                    Formations React.js gratuite
-                    </Button>
-                </div>
-
-            </Container>
+export const CallToActionView = () => {
+  return (
+    <div className="relative overflow-hidden bg-primary">
+      <Container className="py-20">
+        <div className="relative z-10 max-w-3xl space-y-5">
+          <Typography variant="h2" theme="white" component="h2">
+            N'attend pas pour développer tes compétences...
+          </Typography>
+          <div>
+            <Button
+              variant="success"
+              baseUrl="/#"
+              linktype={LinkTypes.EXTERNAL}
+            >
+              Formations React.js gratuite
+            </Button>
+          </div>
         </div>
-    )
-}
+        <div>
+            <Image width={1210} height={12010} src="assets/svg/Group 52.svg" alt="bomb.."/>
+        </div>
+      </Container>
+    </div>
+  );
+};
